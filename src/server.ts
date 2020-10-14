48f3b22f38df8462e1ee43824df3cab50b0d621f
Evandro Neris <evandroneris95@hotmail.com>
2020-10-14 19:32:19 -0300

    building backend

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
 
+app.use(express.json());
 
 app.get('/users', (req, res) => {
   res.status(200).json({ message: 'teste' });
